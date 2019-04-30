cc9f58b83c4bac661a41bdaa3e2fe915a68ef6c3
Morgan Fox <morganfox61297@gmail.com>
2019-04-30 16:15:41 -0400

    as far as i got

--- app/main.js
+++ app/main.js
@@ -1,17 +1,26 @@
 function depositMoney() {
   let amount = prompt('How much do you want to deposit?')
+  let history = document.querySelector('#history')
   let bal = document.querySelector('#bank-balance')
   let currentBal = bal.innerText
   let total = parseInt(currentBal) + parseInt(amount)
   bal.innerText = total
+  history.innerHTML =+ '<p>You deposited' + amount + '</p>'
 }
 
 function withdrawMoney() {
   let amount = prompt('How much do you want to withdraw?')
-  let history = document.querySelector('#history')
+  let history = document.querySelector('#history
+  let OD = document.querySelector('odProtect')
   let bal = document.querySelector('#bank-balance')
   let currentBal = bal.innerText
   let total = parseInt(currentBal) - parseInt(amount)
+  if (OD.checked() == true) {
+    alert('Overdraft Protected!')
+  } else {
+    bal.innerText = total
+  }
+
   bal.innerText = total
   if (total <= 0) {
     bal.classList.add('negative-balance')
